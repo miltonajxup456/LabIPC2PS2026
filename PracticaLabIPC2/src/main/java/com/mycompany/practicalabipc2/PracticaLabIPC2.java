2328f72f0f754d8ef111b6bedf3941e8151a4bb0
MilLin_115 <miltonajxup456@gmail.com>
2026-02-03 20:06:32 -0600

    Subiendo

---
+++ PracticaLabIPC2/src/main/java/com/mycompany/practicalabipc2/PracticaLabIPC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicalabipc2;
+
+/**
+ *
+ * @author millin-115
+ */
+public class PracticaLabIPC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
